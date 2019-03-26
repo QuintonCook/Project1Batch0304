81fcbeb8348b9a46ec685b8d375b5f58fb5f529e
QuintonCook <Quintoncook97@hotmail.com>
2019-03-26 16:20:52 -0500

    removing for a second

--- Project1-0304/src/main/java/force/authenticateDAO/AuthenticateDAO.java
+++
@@ -1,8 +0,0 @@
-package force.authenticateDAO;
-
-import force.TransferObjects.ConnectionInformation;
-import force.TransferObjects.Employee;
-
-public interface AuthenticateDAO extends ConnectionInformation {
-	Employee authenticate(String username, String password);
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Project1Batch0304
-Quinton and Yousef's ERS system
