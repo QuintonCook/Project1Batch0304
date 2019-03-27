5b41eaf80897df871a37d5abeaa61c93077c4ab6
YousefGM <yosmohd@gmail.com>
2019-03-27 17:09:28 -0500

    updates

--- Project1-0304/src/main/webapp/JS/admin.js
+++ Project1-0304/src/main/webapp/JS/admin.js
@@ -2,58 +2,20 @@
  * 
  */
 
-window.onload = function() {
-	loadTable();
-};
-document.getElementById("approvebtn").addEventListener("click", function() {
-	loadTable();
-});
-document.getElementById("denybtn").addEventListener("click", function() {
-	loadTable();
+document.getElementById('approvebtn').addEventlistener("onclick", function() {
+	makechanges(1);
 });
 
-document.getElementById("submitrequestbtn").addEventListener("click",
-		function() {
-			loadTable();
-		});
-
-function updatePage(reimbursements) {
-	for (i = 0; i < reimbursements.length; i++) {
-		let newrow = document.createElement("tr")
-		let row = reimbursement[i];
-
-		for (j = 0; j < row.length; j++) {
-			newrow.appendChild("td").textContent(row[j]);
+function makechanges(statusId) {
+	let forms = document.getElementsByTagName('Form')
+	for (i = 0; i < forms.length; i++) {
+		let c = forms[i].querySelectorAll('input[type="checkbox"]');
+		if (c[0].checked) {
+			let action = forms[i].getAttribute("action")
+			forms[i].setAttribute("action", action + statusId)
+			forms[i].submit();
 		}
 
-		document.getElementById("infotable").appendChild(newrow);
 	}
-}
 
-      document.getElementByTag("td").rows[0].cells.length;
-      cell1.innerHTML = "reimbursements";
-      
-
-function loadTable() {
-	let xhttp = new XMLHttpRequest();
-	if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-		let reimbursements = JSON.parse(xhttp.responseText);
-		updatePage();
-	}
-	xhttp.open("GET", '/admin ? command = ReviewReimbursement NewStatus = ?', true);
-
-	xhttp.send();
-}
-
-function otherpeople() {
-	let xhttp = new XMLHttpRequest();
-	if (xhttp.readystate === 4 && xhttp.status == 200) {
-
-		let reimbursements = JSON.parse(xhttp.responseText);
-		updatePage();
-	}
-	xhttp.open("GET", "/admin", true);
-
-	xhttp.send();
 }
